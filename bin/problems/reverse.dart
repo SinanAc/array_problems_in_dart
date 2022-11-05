c2e184e6a8b7827f7b74b6f0ec31daf78b538fc1
Sinan <sinanac124@gmail.com>
2022-11-05 23:34:18 +0530

    string reversed using for loop

--- bin/problems/reverse.dart
+++ bin/problems/reverse.dart
@@ -2,6 +2,7 @@
 // ======>>  TO REVERSE AN ARRAY 
 //
 void main(){
+  // -->> reversing an array
   List<int> arr = [1,2,3,4,5,6];
   for(int i = 0; i < arr.length/2; i++){
     int temp = arr[i];
@@ -9,4 +10,12 @@ void main(){
     arr[arr.length-1-i] = temp;
   }
   print(arr);
+
+  // -->> reversing a string
+  String str = 'Hello';
+  String reversed = '';
+  for(int i = str.length-1; i >=0; i--){
+    reversed = reversed+str[i];
+  }
+  print(reversed) ;
 }
