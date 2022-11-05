8e23a0fcbcd5db60c1f956c7f00ef10c13ffb4b1
Sinan <sinanac124@gmail.com>
2022-11-05 23:39:37 +0530

    completed string swapping

--- bin/problems/swapping.dart
+++ bin/problems/swapping.dart
@@ -2,17 +2,26 @@
 // ======>>  TO SWAP TWO ARRAYS
 //
 void main() {
+  // -->> array swapping
   List<int> array1 = [10,20,30,40,50];
   List<int> array2 = [11,22,33,44,55];
- 
+
   for (int i=0 ; i<array1.length ;i++){
     int temp = array1[i];
     array1[i]= array2[i];
     array2[i]= temp;
   }
- 
+
   print('Arrays after swapping');
   print('Array1 : $array1');
   print('Array 2 : $array2');
- 
+
+  // -->> string swapping without using third variable
+  String a = 'hello';
+  String b = 'world';
+  a = a + b;
+  b = a.substring(0, a.length - b.length);
+  a = a.substring(a.length - b.length, a.length);
+  print(a);
+  print(b);
 }
