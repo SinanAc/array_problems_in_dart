e0fb4122c3bf3ab5c207a379076d59f082b20a1d
Sinan <sinanac124@gmail.com>
2022-11-06 12:56:13 +0530

    done finding largest values

--- bin/problems/find_largest.dart
+++ bin/problems/find_largest.dart
@@ -13,5 +13,15 @@ void main(){
     print('Largest value in this array is : $largest'); 
 
   // -->> to fing the second largest value in the array  
+      for(int i=0; i<arr.length-1; i++) {
+        for(int j=i+1; j<arr.length; j++) {
+          if(arr[i]<arr[j]){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+          }
+        }
+    }
+    print('Second largest value in this array is : ${arr[1]}'); 
   
 }
