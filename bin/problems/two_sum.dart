43dffbf668af9daa3f346c60e1ac9fe5f7986ba7
Sinan <sinanac124@gmail.com>
2022-11-25 20:47:16 +0530

    two sum

---
+++ bin/problems/two_sum.dart
@@ -0,0 +1,18 @@
+void main() {
+  print(Solution.twoSum([2, 4, 6, 7, 1], 11));
+}
+
+class Solution {
+  static List<int> twoSum(List<int> nums, int target) {
+    List<int> result = [];
+    for (int i = 0; i < nums.length - 1; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          result.addAll([nums[i], nums[j]]);
+          return result;
+        }
+      }
+    }
+    return result;
+  }
+}
