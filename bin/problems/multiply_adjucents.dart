0493b7f5bdbe19d70c0eef512882b287fb45db28
Sinan <sinanac124@gmail.com>
2022-11-25 22:50:49 +0530

    adjacents

--- bin/problems/multiply_adjucents.dart
+++ bin/problems/multiply_adjucents.dart
@@ -1,11 +1,17 @@
 //
-// ======>>  TO MULTIPLY THE ADJUCENT VALUES 
+// ======>>  TO MULTIPLY THE ADJUCENT VALUES
 //
-void main(){
-  List<int> arr = [1,2,3,4,5,6];
-  List<int> results = [];
-    for(int i=0; i<arr.length -1; i++) {
-        results.add(arr[i]*arr[i+1]);
+void main() {
+  List<int> arr = [1, 2, 3, 4, 5, 6];
+  Adjacents.multiplyAdjucents(arr);
+}
+
+class Adjacents {
+  static void multiplyAdjucents(List<int> arr) {
+    List<int> results = [];
+    for (int i = 0; i < arr.length - 1; i++) {
+      results.add(arr[i] * arr[i + 1]);
     }
-    print('Answer : $results'); 
+    print('Answer : $results');
+  }
 }
