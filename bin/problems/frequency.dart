3fccbc15f81c570f2cb817c9b8d82d1919ecdf23
Sinan <sinanac124@gmail.com>
2022-11-26 11:50:28 +0530

    frequency done

--- bin/problems/frequency.dart
+++ bin/problems/frequency.dart
@@ -1,21 +1,28 @@
 //
 // ======>>  TO FIND THE FREQUENCY OF EACH ELEMENT
 //
- void main() {
-  List<String> arr = ['A','P','P','L','E'];
-  List<int> temp = List.generate(arr.length, (index) => -1);
+void main() {
+  List<String> arr = ['A', 'P', 'P', 'L', 'E'];
+  // --->>> to get frequency of eacg characters in String/List<String>
+  Frequency.getFrequencyOfString(arr);
+}
+
+class Frequency {
+  static void getFrequencyOfString(List<String> arr) {
+    List<int> temp = List.generate(arr.length, (index) => -1);
     for (int i = 0; i < arr.length; i++) {
       int count = 0;
-    for (int j = 0; j <arr.length; j++) {
-      if (arr[i] == arr[j]) {
-        count++;
-        if(count>1){
-          temp[j]=0;
+      for (int j = 0; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+          count++;
+          if (count > 1) {
+            temp[j] = 0;
+          }
         }
       }
-    }
-    if(temp[i]!=0){
-      print(arr[i]+count.toString());
+      if (temp[i] != 0) {
+        print(arr[i] + count.toString());
+      }
     }
   }
- }
+}
