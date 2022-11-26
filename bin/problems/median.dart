67c2a3c336aeb131ee8108df5681fea13c9bb32b
Sinan <sinanac124@gmail.com>
2022-11-26 11:44:05 +0530

    median done

--- bin/problems/median.dart
+++ bin/problems/median.dart
@@ -1,26 +1,29 @@
 //
-// ======>>  TO FIND MEDIAN
+// ======>>  TO FIND MEDIAN & AVERAGE
 //
-void main(){
-  List<int> array = [23,45,76,89,106,344,789];
-  print(avrage(array));
+void main() {
+  List<int> array = [23, 45, 76, 89, 106, 344, 789];
+  // -->> to find the median of an array
+  print(MedianAndAverage.findMedian(array));
+  // -->> to find average
+  print(MedianAndAverage.avrage(array));
 }
 
-// -->> to find the median of an array
-int findMedian(List<int> array){
-    if(array.length%2!=0){
-      return array[(array.length)~/2.toInt()];
-    }else{
-      int a = ((array.length-1)~/2).toInt();
-      return ((array[a]+array[a+1])~/2).toInt();
+class MedianAndAverage {
+  static int findMedian(List<int> array) {
+    if (array.length % 2 != 0) {
+      return array[(array.length) ~/ 2.toInt()];
+    } else {
+      int a = ((array.length - 1) ~/ 2).toInt();
+      return ((array[a] + array[a + 1]) ~/ 2).toInt();
     }
-}
+  }
 
-// -->> to find average
-double avrage(List<int> array){
+  static double avrage(List<int> array) {
     double sum = 0;
-    for(int i=0;i<array.length;i++){
-      sum = sum+array[i];
+    for (int i = 0; i < array.length; i++) {
+      sum = sum + array[i];
     }
-    return sum/array.length;
+    return sum / array.length;
+  }
 }
