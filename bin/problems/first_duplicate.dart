83d81f2ca1d13bfb0fc79f06af3216299d840b77
Sinan <sinanac124@gmail.com>
2022-11-27 20:20:22 +0530

    duplicates

--- bin/problems/first_duplicate.dart
+++ bin/problems/first_duplicate.dart
@@ -1,13 +1,19 @@
 //
 // ======>>  TO FIND THE FIRST DUPLICATE ELEMENT
 //
- void main() {
+void main() {
   List<int> arr = [1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1];
+  Duplicate.getFirstDuplicate(arr);
+}
+
+class Duplicate {
+  static void getFirstDuplicate(List<int> arr) {
     for (int i = 1; i < arr.length; i++) {
-    for (int j = i-1; j >=0; j--) {
-      if (arr[i] == arr[j]) {
-        print (arr[i]);
+      for (int j = i - 1; j >= 0; j--) {
+        if (arr[i] == arr[j]) {
+          print(arr[i]);
+        }
       }
     }
   }
- }
+}
