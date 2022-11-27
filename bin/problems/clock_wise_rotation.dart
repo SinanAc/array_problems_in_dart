63b7b8444a7016420807b0e59a3c682bc5339ba9
Sinan <sinanac124@gmail.com>
2022-11-27 20:28:44 +0530

    rotating array

--- bin/problems/clock_wise_rotation.dart
+++ bin/problems/clock_wise_rotation.dart
@@ -3,13 +3,19 @@
 //
 void main() {
   List<String> arr = ['A', 'P', 'P', 'L', 'E'];
-  int rotations = 2;
-  for (int i = 0; i < rotations ; i++) {
-    String temp = arr[arr.length - 1];
-    for (int j = arr.length - 1; j > 0; j--) {
-      arr[j]= arr[j-1];
+  Rotation.rotateArrayInClockWise(arr);
+}
+
+class Rotation {
+  static void rotateArrayInClockWise(List<String> arr) {
+    int rotations = 2;
+    for (int i = 0; i < rotations; i++) {
+      String temp = arr[arr.length - 1];
+      for (int j = arr.length - 1; j > 0; j--) {
+        arr[j] = arr[j - 1];
+      }
+      arr[0] = temp;
     }
-    arr[0]=temp;
+    print(arr);
   }
-  print(arr);
 }
