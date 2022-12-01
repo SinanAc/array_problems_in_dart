f89b5a0381d9aa8bc3a7bd566f273124dc46d867
Sinan <sinanac124@gmail.com>
2022-12-01 19:20:48 +0530

    largers

--- bin/problems/find_largest.dart
+++ bin/problems/find_largest.dart
@@ -5,14 +5,28 @@ void main() {
   List<int> arr = [1, 2, 3, 5, 24, 8, 4, 3, 4];
   // -->> to find the largest value in the array
   LargestValue.getLargetValue(arr);
+  // ======================================================
+
   // -->> to find the second largest value in the array
   LargestValue.getSecondLarget(arr);
+  // ======================================================
+
   // -->> to find the peak value which is larger than neighbours
   LargestValue.getPeakValueWhichIsLargerThanNeighbours(arr);
+  // ======================================================
+
   // -->> another method to find the largest value in the array
   // getting the largest value without using third variable
   // taking the largest value to the 0th position
   LargestValue.getLargestByTakingToZeroth(arr);
+  // ======================================================
+
+  // Given an array A of positive integers.
+  // The task is to find the leaders in the array.
+  // An element of array is leader if it is greater than or equal to all the elements to its right side.
+  // The rightmost element is always a leader.
+  // A[] = {16,17,4,3,5,2}
+  // Output: 17 5 2
 }
 
 class LargestValue {
