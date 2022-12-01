689c58b8b528fbe74ab28bf7922b9bfb59237b7e
Sinan <sinanac124@gmail.com>
2022-12-01 19:34:58 +0530

    leaders done

--- bin/problems/find_largest.dart
+++ bin/problems/find_largest.dart
@@ -22,11 +22,12 @@ void main() {
   // ======================================================
 
   // Given an array A of positive integers.
+  List<int> a = [16, 17, 4, 3, 5, 2];
   // The task is to find the leaders in the array.
   // An element of array is leader if it is greater than or equal to all the elements to its right side.
   // The rightmost element is always a leader.
-  // A[] = {16,17,4,3,5,2}
-  // Output: 17 5 2
+  // Expected Output: 17 5 2
+  LargestValue.getLeaders(a);
 }
 
 class LargestValue {
@@ -37,7 +38,7 @@ class LargestValue {
         largest = arr[i];
       }
     }
-    print('Largest value in this array is : $largest');
+    print('Largest value in this array is: $largest');
   }
 
   static void getSecondLarget(List<int> arr) {
@@ -50,27 +51,45 @@ class LargestValue {
         }
       }
     }
-    print('Second largest value in this array is : ${arr[1]}');
+    print('Second largest value in this array is: ${arr[1]}');
   }
 
   static void getPeakValueWhichIsLargerThanNeighbours(List<int> arr) {
     if (arr[0] > arr[1]) {
-      print('The peak value which is larger than neighbours is : ${arr[0]}');
+      print('The peak value which is larger than neighbours is: ${arr[0]}');
       return;
     }
     for (int i = 1; i < arr.length - 1; i++) {
       if (arr[i] > arr[i - 1] && arr[i] > arr[i + 1]) {
-        print('The peak value which is larger than neighbours is : ${arr[i]}');
+        print('The peak value which is larger than neighbours is: ${arr[i]}');
         return;
       }
     }
     if (arr[arr.length - 1] > arr[arr.length - 2]) {
       print(
-        'The peak value which is larger than neighbours is : ${arr[arr.length - 1]}',
+        'The peak value which is larger than neighbours is: ${arr[arr.length - 1]}',
       );
       return;
     }
     print('No valid peak value!!');
+  }
+
+  static void getLeaders(List<int> arr) {
+    List leaders = [];
+    for (int i = 0; i < arr.length - 1; i++) {
+      bool isLeader = true;
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] < arr[j]) {
+          isLeader = false;
+          break;
+        }
+      }
+      if (isLeader) {
+        leaders.add(arr[i]);
+      }
+    }
+    leaders.add(arr[arr.length - 1]);
+    print('Leaders: $leaders');
   }
 
   static void getLargestByTakingToZeroth(List<int> arr) {
@@ -79,6 +98,6 @@ class LargestValue {
         arr[0] = arr[i];
       }
     }
-    print('Largest value in this array is : ${arr[0]}');
+    print('Largest value in this array is: ${arr[0]}');
   }
 }
